e634ad29931938942bfecc8a91dfa72f9d7693bb
刘依林 <1628530447@qq.com>
2019-04-24 10:36:50 +0800

    aa

--- demo/src/main/java/com/weichu/jinxiaocun/demo/comment/E.java
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/comment/E.java
@@ -4,16 +4,5 @@ public class E {
 
     private String lanwanglong;
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    private String lan;
 }
