6d6a0ad03f80123d87224b9f9498262c244167af
hwwhwww <1907375750@qq.com>
2019-04-24 09:25:12 +0800

    骨架

---
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.weichu.jinxiaocun.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("第一个团队项目");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
