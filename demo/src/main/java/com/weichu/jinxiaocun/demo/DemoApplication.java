75f63328bced29c77f39395d78ad1c6ffef05bee
hwwhwww <1907375750@qq.com>
2019-04-24 13:40:56 +0800

    用户

--- demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
@@ -7,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("第一个团队项目");
-        System.out.println("123");
-        System.out.println("1111111111111111111");
-        System.out.println("yayaya");
-        System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
-        System.out.println("23223");
         SpringApplication.run(DemoApplication.class, args);
     }
 
