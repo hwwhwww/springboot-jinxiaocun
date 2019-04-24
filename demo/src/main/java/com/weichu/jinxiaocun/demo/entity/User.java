030d7e441efb0b77824b5259aefd6fb3bcd3cfc8
刘依林 <1628530447@qq.com>
2019-04-24 10:29:36 +0800

    实体类

--- demo/src/main/java/com/weichu/jinxiaocun/demo/entity/User.java
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/entity/User.java
@@ -1,4 +1,32 @@
 package com.weichu.jinxiaocun.demo.entity;
 
 public class User {
+
+    private int userID;
+    private String userName;
+    private String add;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
 }
