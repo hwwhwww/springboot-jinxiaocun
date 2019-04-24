9ea47981f4886b40fcd8b2f0d98c367de749f7d4
xb12369 <2940366047@qq.com>
2019-04-24 10:23:45 +0800

    11111111111111111111

--- demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         System.out.println("第一个团队项目");
         System.out.println("123");
         System.out.println("1111111111111111111");
+        System.out.println("yayaya");
         SpringApplication.run(DemoApplication.class, args);
     }
 
