703f116e994b96efb269abfde9c3de11f46a1108
hwwhwww <1907375750@qq.com>
2019-04-26 10:28:04 +0800

    eeee

--- demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("提价");
         SpringApplication.run(DemoApplication.class, args);
     }
 
