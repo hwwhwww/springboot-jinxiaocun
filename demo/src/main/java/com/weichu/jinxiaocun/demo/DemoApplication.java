9498375bab15f7fb0bf80a823c47180163d851b4
hwwhwww <1907375750@qq.com>
2019-04-26 10:10:16 +0800

    application

--- demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
+++ demo/src/main/java/com/weichu/jinxiaocun/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
     public static void main(String[] args)
     {
         System.out.println("123");
+
         SpringApplication.run(DemoApplication.class, args);
     }
 
